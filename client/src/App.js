2775aaf69e8452f9d93a453f1e1ae735759e4afb
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-05 11:55:47 +0600

    New Package MUI Install to Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddUser from './Components/AddUser';
 
 function App() {
   return (
     <div className="App">
-
+      <AddUser></AddUser>
     </div>
   );
 }
