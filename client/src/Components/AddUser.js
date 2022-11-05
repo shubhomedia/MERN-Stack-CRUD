cca21bfdb125cf7fcc177ea177b5c3ee94b68a00
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-05 13:14:20 +0600

    Update Add User Form

--- client/src/Components/AddUser.js
+++ client/src/Components/AddUser.js
@@ -1,9 +1,27 @@
+import { FormControl, FormGroup, Input, InputLabel } from '@mui/material';
 import React from 'react';
 
 const AddUser = () => {
     return (
         <div>
-            <h2>Add User Page</h2>
+            <FormGroup>
+                <FormControl>
+                    <InputLabel>Name</InputLabel>
+                    <Input></Input>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>UserName</InputLabel>
+                    <Input></Input>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Email</InputLabel>
+                    <Input></Input>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Phone</InputLabel>
+                    <Input></Input>
+                </FormControl>
+            </FormGroup>
         </div>
     );
 };
