c2407320df75779f0d3523017247bb769b030aa5
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-06 07:39:00 +0600

    Update App.js
    App Page Code Change

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import AllUsers from './Components/AllUsers';
 import AddUser from './Components/AddUser';
+import EditUser from './Components/EditUser';
 import NavBar from './Components/NavBar';
-import Home from './Components/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AllUser from './Components/AllUser';
 import NotFound from './Components/NotFound';
+import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <NavBar></NavBar>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/alluser' element={<AllUser />} />
-          <Route path='/adduser' element={<AddUser />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Router >
-    </div >
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="all" element={<AllUsers />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
