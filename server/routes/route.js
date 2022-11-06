4be441f619afa529fe1a0cb71d01de5035e6c3d1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-06 07:41:25 +0600

    user File Update

--- server/routes/route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { addUser } from '../Controller/user-controller.js';
-
-const router = express.Router();
-
-router.post('/add', addUser);
-
-export default router;
