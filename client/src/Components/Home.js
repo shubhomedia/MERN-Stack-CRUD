5f7054593ceb63b6c8768401ef690a7c44f83017
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-06 07:40:19 +0600

    Home Page Update

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import { Box, Typography, styled } from '@mui/material';
 
-const Home = () => {
+import Youtube from '../Assets/Images/youtube.png';
+import InstaTele from '../Assets/Images/InstaTele.jpeg';
+
+const Header = styled(Box)`
+    margin: 50px;
+    & > div {
+        margin-top: 50px;
+    }
+`;
+
+const Image = styled('img')({
+    width: '50%',
+    height: '50%'
+});
+
+const CodeForInterview = () => {
+
     return (
-        <div>
-            <h2>This is Home</h2>
-        </div>
-    );
-};
+        <Header>
+            <Typography variant="h4">Code for Interview</Typography>
+            <Box style={{ display: 'flex' }}>
+                <Image src={Youtube} />
+                <Image src={InstaTele} />
+            </Box>
+        </Header>
+    )
+}
 
-export default Home;
+export default CodeForInterview;
